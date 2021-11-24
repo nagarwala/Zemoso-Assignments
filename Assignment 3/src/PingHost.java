1a5ffe2dfebb41260a03314db60108cce9415fa4
NITESH AGARWALA <80337559+nagarwala@users.noreply.github.com>
2021-11-24 09:44:45 +0530

    Completed Assignment 3

---
+++ Assignment 3/src/PingHost.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class PingHost {
+
+    public static void runSystemCommand(String command) {
+
+        try {
+            Process p = Runtime.getRuntime().exec(command);
+            BufferedReader inputStream = new BufferedReader(
+                    new InputStreamReader(p.getInputStream()));
+
+            String s = "";
+            // reading output stream of the command
+            while ((s = inputStream.readLine()) != null) {
+                System.out.println(s);
+                //p.destroy();
+            }
+
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public static void main(String[] args) {
+
+        String ip;
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the ip address : ");
+        ip =in.nextLine();
+        runSystemCommand("ping " + ip);
+
+    }
+}
